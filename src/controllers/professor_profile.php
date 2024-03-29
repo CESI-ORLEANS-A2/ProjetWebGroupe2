c1263f6f5c8b68c6a9720ef25b1ff9238985b33c
ayasalem123 <112411536+ayasalem123@users.noreply.github.com>
2024-03-29 16:30:03 +0100

    Update professor_profile.php

--- src/controllers/professor_profile.php
+++ src/controllers/professor_profile.php
@@ -1,15 +1,1 @@
-<?php
 
-require_once('../src/modules/ControllerBase.php');
-
-class Controller extends ControllerBase {
-    public function __construct($router) {
-        parent::__construct($router);
-    }
-
-    public function run() {
-        return $this->render('professor_profile.twig', array(
-            'config' => $this->config,
-        ));
-    }
-};

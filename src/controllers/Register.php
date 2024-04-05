d08a398745ba35d872995b9e24b63b2ac682af14
Matth-MR <150125538+Matth-MR@users.noreply.github.com>
2024-04-05 11:48:08 +0200

    Add files via upload

---
+++ src/controllers/Register.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once('../src/modules/ControllerBase.php');
+
+class Controller extends ControllerBase {
+    public function __construct($router) {
+        parent::__construct($router);
+    }
+
+    public function run() {
+        return $this->render('register.twig', array(
+        ));
+    }
+};
